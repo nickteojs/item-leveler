e47af505478a18a4c2f42fb481f32307373e8042
Nick Teo <finalmoweyy@gmail.com>
2021-12-20 22:49:30 +0800

    Changed meta tags, edited readme

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
 
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false
+}
+
+const theme = extendTheme({config})
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-    <ColorModeScript initialColorMode="dark"/>
-    <App />
+    <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+      <App />
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
